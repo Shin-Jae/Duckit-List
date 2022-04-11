e386173924df4fd03b51c5be64d14f1d85569199
Celeste Winterton <celestewinterton@gmail.com>
2022-04-11 10:25:19 -0700

    Created database and env files

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// const db = require('../db/models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
