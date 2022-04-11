931ccafa71c817ff1d4ba0e7ca9663cdc40d1b57
Jae Shin <7jaeshin@gmail.com>
2022-04-11 16:18:48 -0400

    auth complete

---
+++ auth.js
@@ -0,0 +1,39 @@
+const db = require('./db/models');
+
+const loginUser = (req, res, user) => {
+    req.session.auth = { userId: user.id };
+};
+
+const logoutUser = (req, res) => {
+    delete req.session.auth;
+};
+
+const requireAuth = (req, res, next) => {
+    if (!res.locals.authenticated) {
+        return res.redirect('/users/login');
+    };
+    return next();
+};
+
+const restoreUser = async (req, res, next) => {
+    if (req.session.auth) {
+        const { userId } = req.session.auth;
+
+        try {
+            const user = await db.User.findByPk(userId);
+            if (user) {
+                res.locals.authenticated = true;
+                res.locals.user = user;
+                next();
+            };
+        } catch (error) {
+            res.locals.authenticated = false;
+            next(error);
+        }
+    } else {
+        res.locals.authenticated = false;
+        next();
+    };
+};
+
+module.exports = { loginUser, logoutUser, requireAuth, restoreUser };
