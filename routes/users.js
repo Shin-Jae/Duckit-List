7369c45198425fa836a735e94b07f717bcf1f361
Celeste Winterton <celestewinterton@gmail.com>
2022-04-12 10:19:05 -0700

    Have signup form redirect home and crete new user

--- routes/users.js
+++ routes/users.js
@@ -58,7 +58,7 @@ const userValidators = [
     .isEmail()
     .withMessage('Please provide a valid email address')
     .custom((value) => {
-      return db.User.findOne({ where: { emailAddress: value } })
+      return db.User.findOne({ where: { email: value } })
         .then((user) => {
           if (user) {
             return Promise.reject('The provided Email Address is already in use by another account');
@@ -92,7 +92,7 @@ router.post('/signup', csrfProtection, userValidators, asyncHandler(async (req, 
     lastName,
     username,
     password,
-    confirmedPassword,
+    hashedPassword,
   } = req.body;
 
   const user = db.User.build({
@@ -109,7 +109,7 @@ router.post('/signup', csrfProtection, userValidators, asyncHandler(async (req, 
     user.hashedPassword = hashedPassword;
     await user.save();
     loginUser(req, res, user);
-    res.redirect('/');
+    res.redirect('/home');
   } else {
     const errors = validatorErrors.array().map((error) => error.msg);
     res.render('signup', {
@@ -118,7 +118,7 @@ router.post('/signup', csrfProtection, userValidators, asyncHandler(async (req, 
       errors,
       csrfToken: req.csrfToken(),
     });
-    req.session.auth = { userId: user.id, username: user.username }
+    req.session.auth = { userId: user.id, username: user.username, firstName: user.firstName, lastName: user.lastName, email: user.email }
     res.redirect('/home')
   }
 }));
