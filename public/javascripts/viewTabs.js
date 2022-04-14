c64139551e9547b97974b15e5e7c19da6050593b
vee-alianza <vee.alianza@gmail.com>
2022-04-14 12:43:30 -0600

    working on how to grab data and put into tabs

---
+++ public/javascripts/viewTabs.js
@@ -0,0 +1,28 @@
+
+const likesBtn = document.querySelector(".likes");
+const completedTasksBtn = document.querySelector(".pets");
+const commentsBtn = document.querySelector(".comments");
+const likesTable = document.querySelector(".petLikes");
+const completedTasksTable = document.querySelector(".petsTable");
+const commentsTable = document.querySelector(".commentsTable");
+
+likesBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    likesTable.style.display = "flex";
+    petsTable.style.display = "none";
+    commentsTable.style.display = "none";
+});
+
+petsBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    petsTable.style.display = "flex";
+    likesTable.style.display = "none";
+    commentsTable.style.display = "none";
+});
+
+commentsBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    commentsTable.style.display = "flex";
+    likesTable.style.display = "none";
+    petsTable.style.display = "none";
+});
