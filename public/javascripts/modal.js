d237b8685b7e768b28554c67514efb770c3e9ef5
vee-alianza <vee.alianza@gmail.com>
2022-04-15 08:37:44 -0600

    creating modal

---
+++ public/javascripts/modal.js
@@ -0,0 +1,19 @@
+// container for modal
+const modal = document.querySelector(".modal");
+
+// "SUMMON MODAL" button on viewlist.pug
+document.querySelector("#render-modal").addEventListener("click", e => {
+    modal.classList.toggle("show-modal");
+});
+
+// "x" button to close modal
+document.querySelector(".close-button").addEventListener("click", e => {
+    modal.classList.toggle("show-modal");
+});
+
+// click anywhere "outside the modal"
+window.addEventListener("click", e => {
+    if (e.target === modal) {
+        modal.classList.toggle("show-modal");
+    }
+});
